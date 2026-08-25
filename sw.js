46bc6e10953bd5fc1510ca9bfbe8ca61cd8c83f9
tanupon66 <tanupon66@gmail.com>
2026-08-25 18:13:04 +0700

    Refresh PWA cache for compact UI and optimizer menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='xray-grr-v4-worker-safe-parser';
-const ASSETS=['./','./index.html','./styles.css','./parser-shim.js','./core.js','./app.js','./optimizer.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='xray-grr-v5-compact-ui';
+const ASSETS=['./','./index.html','./styles.css','./ui.css','./parser-shim.js','./core.js','./app.js','./optimizer.js','./ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
