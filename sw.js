55dcb8eb2244418afef9d459a4c54e9f6deff538
tanupon66 <tanupon66@gmail.com>
2026-08-25 19:17:24 +0700

    Refresh cache for mobile optimizer UI fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='xray-grr-v7-study-modes';
+const CACHE='xray-grr-v8-mobile-optimizer-fix';
 const ASSETS=['./','./index.html','./styles.css','./ui.css','./parser-shim.js','./core.js','./app.js','./ui.js','./study-mode.js','./optimizer.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
