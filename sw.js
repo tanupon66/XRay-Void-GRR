ae9eb5c61da77e1a0a6b7d1defdd71b790a71179
tanupon66 <tanupon66@gmail.com>
2026-09-03 14:07:02 +0700

    Refresh PWA cache for optimizer full-data scope fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='xray-grr-v9-varcomp-minitab-aligned';
+const CACHE='xray-grr-v10-optimizer-full-data-scope';
 const ASSETS=['./','./index.html','./styles.css','./ui.css','./parser-shim.js','./core.js','./app.js','./ui.js','./study-mode.js','./optimizer.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
